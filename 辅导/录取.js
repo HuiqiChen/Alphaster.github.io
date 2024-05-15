2629420b8a856a5ab64c943844a4ba7273e5ff85
HuiqiChen <147371799+HuiqiChen@users.noreply.github.com>
2024-05-15 16:03:21 -0700

    Add files via upload

---
+++ 辅导/录取.js
@@ -0,0 +1,8 @@
+// script.js
+function myFunction(img) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = img.src;
+  imgText.innerHTML = img.alt;
+  expandImg.parentElement.style.display = "block";
+}
