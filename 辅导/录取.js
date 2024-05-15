328b43a4ab56eddfeada4ddb1d125aac4bdb8454
HuiqiChen <147371799+HuiqiChen@users.noreply.github.com>
2024-05-15 16:36:59 -0700

    Delete 辅导 directory

--- 辅导/录取.js
+++
@@ -1,8 +0,0 @@
-// script.js
-function myFunction(img) {
-  var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
-  expandImg.src = img.src;
-  imgText.innerHTML = img.alt;
-  expandImg.parentElement.style.display = "block";
-}
